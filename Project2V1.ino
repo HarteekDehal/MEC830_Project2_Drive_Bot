f8138c787705531c1aa4b2ab95c46baff91403cc
HarteekDehal <43826789+HarteekDehal@users.noreply.github.com>
2023-11-19 20:12:27 -0500

    Add files via upload
    Code stabilizes movement of the car and alters motor speeds to keep the car moving in a straight line. Can be altered in V2 for turning at right angles when landmark is reached.

---
+++ Project2V1.ino
@@ -0,0 +1,240 @@
+#include <Wire.h>
+#include <PID_v1.h>
+
+
+//control pins for left and right motors
+const int rightSpeed = 5; //means pin 9 on the Arduino controls the speed of left motor
+const int leftSpeed = 11;
+const int right1 = 6; //left 1 and left 2 control the direction of rotation of left motor
+const int right2 = 7;
+const int left1 = 9;
+const int left2 = 10;
+#define Button 3
+int onOff=0;
+
+
+
+const int MPU = 0x68; // MPU6050 I2C address
+float AccX, AccY, AccZ; //linear acceleration
+float GyroX, GyroY, GyroZ; //angular velocity
+float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ; //used in void loop()
+float roll, pitch, yaw;
+float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
+float elapsedTime, currentTime, previousTime;
+int c = 0;
+
+const int topSpeed = 50; //max PWM value written to motor speed pin. It is typically 255.
+const int minSpeed = 40; //min PWM value at which motor moves
+float angle; //due to how I orientated my MPU6050 on my car, angle = roll
+//Establish PID values
+double kp = 2, ki = 0.1, kd = 0.1;
+double input = 0, output = 0, setpoint = 0;
+PID myPID(&input, &output, &setpoint, kp, ki, kd, DIRECT);
+
+
+
+int equilibriumSpeed = 60; //rough estimate of PWM at the speed pin of the stronger motor, while driving straight 
+//and weaker motor at maxSpeed
+int leftSpeedVal;
+int rightSpeedVal;
+bool isDriving = false; //it the car driving forward OR rotate/stationary
+bool prevIsDriving = true; //equals isDriving in the previous iteration of void loop()
+bool paused = false; //is the program paused
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();                      // Initialize comunication
+  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
+  Wire.write(0x6B);                  // Talk to the register 6B
+  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
+  Wire.endTransmission(true);        //end the transmission
+  // Call this function if you need to get the IMU error values for your module
+  calibrate();
+  delay(20);
+  pinMode(left1, OUTPUT);
+  pinMode(left2, OUTPUT);
+  pinMode(right1, OUTPUT);
+  pinMode(right2, OUTPUT);
+  pinMode(leftSpeed, OUTPUT);
+  pinMode(rightSpeed, OUTPUT);
+  currentTime = micros();
+   pinMode(Button, INPUT_PULLUP);
+
+     myPID.SetMode(AUTOMATIC); // Set the PID controller mode to AUTOMATIC, enabling automatic control
+  myPID.SetSampleTime(1);
+  myPID.SetOutputLimits(-70, 70); // These limits determine the range of control values that can be applied to the motor
+}
+
+void loop() {
+  // === Read accelerometer (on the MPU6050) data === //
+  readAcceleration();
+  // Calculating Roll and Pitch from the accelerometer data
+  accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorX; //AccErrorX is calculated in the calculateError() function
+  accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorY;
+  
+  // === Read gyroscope (on the MPU6050) data === //
+  previousTime = currentTime;
+  currentTime = micros();
+  elapsedTime = (currentTime - previousTime) / 1000000; // Divide by 1000 to get seconds
+  readGyro();
+  // Correct the outputs with the calculated error values
+  GyroX -= GyroErrorX; //GyroErrorX is calculated in the calculateError() function
+  GyroY -= GyroErrorY;
+  GyroZ -= GyroErrorZ;
+  // Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by sendonds (s) to get the angle in degrees
+  gyroAngleX += GyroX * elapsedTime; // deg/s * s = deg
+  gyroAngleY += GyroY * elapsedTime;
+  yaw += GyroZ * elapsedTime;
+  //combine accelerometer- and gyro-estimated angle values. 0.96 and 0.04 values are determined through trial and error by other people
+  roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
+  pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
+  angle = -roll; //if you mounted MPU6050 in a different orientation to me, angle may not = roll. It can roll, pitch, yaw or minus version of the three
+  //for me, turning right reduces angle. Turning left increases angle.
+Serial.println("Angle: ");
+Serial.println(angle);
+delay(10);
+
+forward();
+
+input=angle;
+myPID.Compute();
+
+output=setpoint+input;
+
+if(output>setpoint){
+  rightSpeedVal=topSpeed+20;
+  leftSpeedVal=minSpeed;
+}
+else if(output<setpoint){
+   leftSpeedVal=topSpeed;
+  rightSpeedVal=minSpeed;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  byte buttonState = digitalRead(Button);
+  
+  if (buttonState == LOW){
+onOff++;
+delay(250);
+  }
+if (onOff=1){
+forward();
+}
+else if(onOff=2){
+stopCar();
+
+}
+*/
+
+
+
+}
+
+
+void calibrate() {
+  // Read accelerometer values 200 times
+  c = 0;
+  while (c < 200) {
+    readAcceleration();
+    // Sum all readings
+    AccErrorX += (atan((AccY) / sqrt(pow((AccX), 2) + pow((AccZ), 2))) * 180 / PI);
+    AccErrorY += (atan(-1 * (AccX) / sqrt(pow((AccY), 2) + pow((AccZ), 2))) * 180 / PI);
+    c++;
+  }
+  //Divide the sum by 200 to get the error value, since expected value of reading is zero
+  AccErrorX = AccErrorX / 200;
+  AccErrorY = AccErrorY / 200;
+  c = 0;
+  // Read gyro values 200 times
+  while (c < 200) {
+    readGyro();
+    // Sum all readings
+    GyroErrorX += GyroX;
+    GyroErrorY += GyroY;
+    GyroErrorZ += GyroZ;
+    c++;
+  }
+  //Divide the sum by 200 to get the error value
+  GyroErrorX = GyroErrorX / 200;
+  GyroErrorY = GyroErrorY / 200;
+  GyroErrorZ = GyroErrorZ / 200;
+  Serial.println("The the gryoscope setting in MPU6050 has been calibrated");
+}
+void readAcceleration() {
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
+  //For a range of +-2g, we need to divide the raw values by 16384, according to the MPU6050 datasheet
+  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value
+  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0; // Y-axis value
+  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
+}
+void readGyro() {
+  Wire.beginTransmission(MPU);
+  Wire.write(0x43);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true);
+  GyroX = (Wire.read() << 8 | Wire.read()) / 131.0;
+  GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
+  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
+}
+
+
+
+
+//Movement
+void stopCar(){
+  digitalWrite(right1, LOW);
+  digitalWrite(right2, LOW);
+  digitalWrite(left1, LOW);
+  digitalWrite(left2, LOW);
+  analogWrite(rightSpeed, 0);
+  analogWrite(leftSpeed, 0);
+}
+void forward(){ //drives the car forward, assuming leftSpeedVal and rightSpeedVal are set high enough
+  digitalWrite(right1, HIGH); //the right motor rotates FORWARDS when right1 is HIGH and right2 is LOW
+  digitalWrite(right2, LOW);
+  digitalWrite(left1, HIGH);
+  digitalWrite(left2, LOW);
+  analogWrite(rightSpeed, rightSpeedVal);
+    analogWrite(leftSpeed, leftSpeedVal);
+}
+void left(){ //rotates the car left, assuming speed leftSpeedVal and rightSpeedVal are set high enough
+  digitalWrite(right1, LOW);
+  digitalWrite(right2, HIGH);
+  digitalWrite(left1, HIGH);
+  digitalWrite(left2, LOW);
+}
+void right(){
+  digitalWrite(right1, HIGH);
+  digitalWrite(right2, LOW);
+  digitalWrite(left1, LOW);
+  digitalWrite(left2, HIGH);
+}
